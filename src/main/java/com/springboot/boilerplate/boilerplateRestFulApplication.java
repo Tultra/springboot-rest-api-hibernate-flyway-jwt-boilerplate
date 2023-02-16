729b775a0d85e8fce2dc5d1d92f93e7dd158f5dd
Tultra <thiago_ultra@yahoo.com.br>
2023-02-16 13:07:05 -0300

    ajustes

--- src/main/java/com/springboot/boilerplate/boilerplateRestFulApplication.java
+++ src/main/java/com/springboot/boilerplate/boilerplateRestFulApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CodeultraResTfulApplication {
+public class boilerplateRestFulApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CodeultraResTfulApplication.class, args);
+		SpringApplication.run(boilerplateRestFulApplication.class, args);
 	}
 
 }
